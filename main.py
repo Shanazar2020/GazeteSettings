c921393b57901165b6f438e64db0904de73bd2a8
Shanazar Nizamaddinov <snizamaddinow@gmail.com>
2023-04-29 22:06:56 +0300

    Database added

--- main.py
+++ main.py
@@ -1,40 +1,53 @@
 import json
-from enum import Enum
 from abc import ABC, abstractmethod
+from typing import Callable
+
+from bs4 import BeautifulSoup, Tag
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
 
 
-def pretty_print(obj):
-    print(json.dumps(obj, indent=4))
-
-
-def get_source_info_from_db(s_id):
-    with open('specific_settings.json', 'r') as f:
-        data = json.load(f)
-        for d in data:
-            yield d
-    # return data[0]
+def get_source_info_from_db(id_):
+    if id_:
+        try:
+            client = MongoClient("localhost", 27017)
+            db = client['news']
+            collection = db['source_settings']
+            document = collection.find_one({"ID": id_})
+            return document
+        except Exception as e:
+            print(e)
+            raise e
+    return None
 
 
 class Settings(ABC):
     class Keys:
         check = 'c'
-        img = 'i'
-        yazar = 'y'
         title = 't'
+        yazar = 'y'
+        img = 'i'
         desc = 'd'
 
     def __init__(self, _id):
         self._id = _id
-        source = self._get_source_info()
+        source = self.__get_source_info()
 
         self._gazete = source.get('gazete')
         self._base_url = source.get('url')
         self._country = source.get('country')
-        self._settings = source.get('settings')
+        self._settings = self._get_settings(source)
+
+    def __get_source_info(self) -> dict:
+        return get_source_info_from_db(self._id)
 
     @abstractmethod
-    def _get_source_info(self) -> dict:
+    def _get_settings(self, sources) -> dict:
         pass
+
+    @property
+    def check(self):
+        return self._settings[self.Keys.check]
 
     @property
     def img(self):
@@ -57,11 +70,12 @@ class Settings(ABC):
         return self._settings
 
 
-class ListSettings(Settings):
+class ArticleListSettings(Settings):
     class ListKeys:
         list = 'l'
         url = 'u'
         url_index = 'u_i'
+        item_limit = 'i_l'
 
     @property
     def list(self):
@@ -75,29 +89,35 @@ class ListSettings(Settings):
     def url_index(self):
         return self._settings[self.ListKeys.url_index]
 
+    @property
+    def item_limit(self):
+        return int(self.settings[self.ListKeys.item_limit])
+
     def __init__(self, s_id):
+        self.mappings = {
+            self.Keys.check: 'TagCheck',
+            self.Keys.img: 'TagImg',
+            self.Keys.yazar: 'TagYazar',
+            self.Keys.title: 'TagTitle',
+            self.Keys.desc: 'TagDesc',
+            self.ListKeys.list: 'TagList',
+            self.ListKeys.url: 'TagUrl',
+            self.ListKeys.url_index: 'TagUrlIndex',
+            self.ListKeys.item_limit: 'itemLimit'
+        }
         super().__init__(s_id)
 
-    def _get_source_info(self) -> dict:
-        source_infos = get_source_info_from_db(self._id)
-        for source_info in source_infos:
-            prev_settings = json.loads(source_info.get('settings', '{}')).get('list', {})
-            source_info['settings'] = {
-                self.Keys.check: prev_settings.get('TagCheck'),
-                self.Keys.img: prev_settings.get('TagImg'),
-                self.Keys.yazar: prev_settings.get('TagYazar'),
-                self.Keys.title: prev_settings.get('TagTitle'),
-                self.Keys.desc: prev_settings.get('TagDesc')
-            }
+    def _get_settings(self, sources) -> dict:
+        new_settings = {}
+        if sources:
+            prev_settings = json.loads(sources.get('settings', '{}')).get('list', {})
+            for new_key, old_key in self.mappings.items():
+                new_settings[new_key] = prev_settings.get(old_key)
 
-            source_info['settings'][self.ListKeys.list] = prev_settings.get('TagList')
-            source_info['settings'][self.ListKeys.url] = prev_settings.get('TagUrl')
-            source_info['settings'][self.ListKeys.url_index] = prev_settings.get('TagUrlIndex')
-
-        return {}
+        return new_settings
 
 
-class ContentSettings(Settings):
+class ArticleContentSettings(Settings):
     class ContentKeys:
         content = 'con'
         is_content_p = 'is_c_p'
@@ -105,6 +125,17 @@ class ContentSettings(Settings):
         remove_tags = 'r_t'
 
     def __init__(self, s_id):
+        self.mappings = {
+            self.Keys.check: 'TagHolder',
+            self.Keys.img: 'TagImg',
+            self.Keys.yazar: 'TagYazar',
+            self.Keys.title: 'TagTitle',
+            self.Keys.desc: 'TagDesc',
+            self.ContentKeys.content: 'TagContent',
+            self.ContentKeys.is_content_p: 'isContentParagraph',
+            self.ContentKeys.is_content_none: 'isContentNone',
+            self.ContentKeys.remove_tags: 'TagsRemove'
+        }
         super().__init__(s_id)
 
     @property
@@ -123,36 +154,332 @@ class ContentSettings(Settings):
     def remove_tags(self):
         return self._settings[self.ContentKeys.remove_tags]
 
-    def _get_source_info(self) -> dict:
-        source_infos = get_source_info_from_db(self._id)
-        for source_info in source_infos:
-            prev_settings = json.loads(source_info.get('settings', '{}')).get('list', {})
-            source_info['settings'] = {
-                self.Keys.check: prev_settings.get('TagHolder'),
-                self.Keys.img: prev_settings.get('TagImg'),
-                self.Keys.yazar: prev_settings.get('TagYazar'),
-                self.Keys.title: prev_settings.get('TagTitle'),
-                self.Keys.desc: prev_settings.get('TagDesc')
-            }
+    def _get_settings(self, sources) -> dict:
+        new_settings = {}
+        if sources:
+            prev_settings = json.loads(sources.get('settings', {})).get('content', {})
+            for new_key, old_key in self.mappings.items():
+                new_settings[new_key] = prev_settings.get(old_key)
 
-            source_info['settings'][self.ContentKeys.content] = prev_settings.get('TagContent')
-            source_info['settings'][self.ContentKeys.is_content_p] = prev_settings.get('isContentParagraph')
-            source_info['settings'][self.ContentKeys.is_content_none] = prev_settings.get('isContentNone')
-            source_info['settings'][self.ContentKeys.remove_tags] = prev_settings.get('TagsRemove')
-
-        return {}
+        return new_settings
 
 
-def process_list_request():
-    settings = ListSettings(1)
-    pretty_print(settings.settings)
+class Cleaner:
+    def __init__(self):
+        self.cleaning_functions = [self.strip_whitespace, self.remove_newlines, self.remove_by_prefix]
+
+    def clean_text(self, text):
+        cleaning_function: Callable
+        for cleaning_function in self.cleaning_functions:
+            text = cleaning_function(text)
+        return text
+
+    def strip_whitespace(self, text):
+        return text.strip()
+
+    def remove_newlines(self, text):
+        return text.replace('\n', '')
+
+    def remove_by_prefix(self, text: str):
+        if text.startswith('By'):
+            return self.strip_whitespace(text[2:])
+        return text
 
 
-def process_content_request():
-    settings = ContentSettings(2)
-    pretty_print(settings.settings)
+class HTMLContent(ABC):
+    cleaner = Cleaner()
+
+    def __init__(self, content, settings: Settings | ArticleListSettings | ArticleContentSettings):
+        self.settings = settings
+        self.content = BeautifulSoup(markup=content, features='lxml')
+
+    def checked(self) -> bool:
+        checked_content = self.content.select_one(self.settings.check)
+        if checked_content:
+            self.content = checked_content
+            return True
+        return False
+
+    def __get_single_text(self, selector):
+        try:
+            # print("Selector:", selector)
+            text = self.content.select_one(selector)
+            if text:
+                # print("Text exists")
+                return text.get_text()
+            # else:
+            # print(self.content)
+        except Exception as e:
+            print(e)
+            return ""
+        return ""
+
+    def __get_single_url(self, selector: str):
+        try:
+            selectors = selector.split(',')
+            for selector in selectors:
+                attr = 'src'
+                if '>' in selector:
+                    selector, attr = selector.split('>')
+                # print("selector: ", selector)
+                # print("attr:", attr)
+                img_tag = self.content.select_one(selector)
+                if img_tag:
+                    img_url = img_tag.get(attr)
+
+                    if img_url:
+                        return img_url
+
+        except Exception as e:
+            print(e)
+        return None
+
+    def get_title(self) -> str:
+        if self.settings.title:
+            # print('title exists')
+            return self.__get_single_text(self.settings.title)
+        return ""
+
+    def _get_yazar(self):
+        try:
+            pass
+        except Exception as e:
+            print(e)
+
+    def get_yazar(self) -> str:
+        if self.settings.yazar:
+            # print("yazar exists")
+            return self.cleaner.clean_text(self.__get_single_text(self.settings.yazar))
+        return ""
+
+    def get_img(self) -> str:
+        if self.settings.img:
+            # print("Img exists")
+            return self.__get_single_url(self.settings.img)
+
+    def get_desc(self):
+        if self.settings.desc:
+            return self.__get_single_text(self.settings.desc)
+
+    @abstractmethod
+    def get_main_content(self):
+        pass
+
+
+class ArticleListHtml(HTMLContent):
+
+    def __init__(self, content, settings: ArticleListSettings):
+        super().__init__(content, settings)
+
+    def __get_list(self, selector) -> list:
+        try:
+            elements = self.content.select(selector)
+            return elements or []
+        except Exception as e:
+            print(e)
+        return []
+
+    def get_item_list(self) -> list['ArticleListHtml']:
+        self.settings: ArticleListSettings
+        results = []
+        if self.settings.list:
+            items = self.__get_list(self.settings.list)
+            limit = min(len(items), self.settings.item_limit)
+            results = [ArticleListHtml(str(items[i]), self.settings) for i in range(limit) if items[i]]
+
+        return results
+
+    def __get_url_at_index(self, selector, index) -> Tag | None:
+        try:
+            element_list = self.__get_list(selector)
+            if element_list and len(element_list) > index:
+                item: Tag
+                item = element_list[index]
+
+                if item and item.has_attr("href"):
+                    return item.get("href")
+
+        except Exception as e:
+            print(e)
+        return None
+
+    def get_main_content(self):
+        self.settings: ArticleListSettings
+        if self.settings.url:
+            return self.__get_url_at_index(self.settings.url, self.settings.url_index)
+
+
+class ArticleContentHtml(HTMLContent):
+
+    def __init__(self, content, settings: ArticleContentSettings):
+        super().__init__(content, settings)
+
+    def __remove(self, selector):
+        try:
+            for removal in self.content.select(selector):
+                if removal:
+                    removal.decompose()
+        except Exception as e:
+            print(e)
+
+    def __get_content(self):
+        try:
+            self.settings: ArticleContentSettings
+            if self.settings.remove_tags:
+                self.__remove(self.settings.remove_tags)
+
+            content_holders = self.content.select(self.settings.content)
+            if content_holders:
+                content = ''.join(holder.get_text() for holder in content_holders if holder)
+                return content
+        except Exception as e:
+            print(e)
+
+    def get_main_content(self):
+        self.settings: ArticleContentSettings
+        if not self.settings.is_content_none and self.settings.content:
+            return self.__get_content()
+        return ""
+
+
+class Response(ABC):
+    title_key = "title"
+    yazar_key = "yazar"
+    img_key = "img"
+    desc_key = "desc"
+
+    def __init__(self):
+        self.data = list()
+        self.content_key = self.get_main_content_key()
+        self._built_data = dict()
+
+    def add_built_data(self):
+        self.data.append(self._built_data)
+        self._built_data = dict()
+
+    def add_data(self, title, yazar, img, desc, content):
+        data = {
+            self.title_key: title,
+            self.yazar_key: yazar,
+            self.img_key: img,
+            self.desc_key: desc,
+            self.content_key: content
+        }
+        self.data.append(data)
+
+    def set_title(self, title):
+        self._built_data[self.title_key] = title
+        return self
+
+    def set_yazar(self, yazar):
+        self._built_data[self.yazar_key] = yazar
+        return self
+
+    def set_img(self, img):
+        self._built_data[self.img_key] = img
+        return self
+
+    def set_desc(self, desc):
+        self._built_data[self.desc_key] = desc
+        return self
+
+    def set_content(self, content):
+        self._built_data[self.content_key] = content
+        return self
+
+    def get_response(self):
+        return [{'data': self.data}]
+
+    @abstractmethod
+    def get_main_content_key(self):
+        pass
+
+
+class ArticleListResponse(Response):
+    key = 'url'
+
+    def __init__(self):
+        super().__init__()
+
+    def get_main_content_key(self):
+        return self.key
+
+
+class ArticleContentResponse(Response):
+    key = 'content'
+
+    def __init__(self):
+        super().__init__()
+
+    def get_main_content_key(self):
+        return self.key
+
+
+class Builder:
+    @staticmethod
+    def build_article_list_request(html: str, source_id: int):
+        settings = ArticleListSettings(source_id)
+        print("Article list builder: ", settings)
+        content = ArticleListHtml(content=html, settings=settings)
+        response = ArticleListResponse()
+        return content, response
+
+    @staticmethod
+    def build_article_content_request(html: str, source_id: int):
+        print("Content builder")
+        settings = ArticleContentSettings(source_id)
+        print(settings.settings)
+        content = ArticleContentHtml(content=html, settings=settings)
+        response = ArticleContentResponse()
+        return content, response
+
+
+def process_request_common(content, response):
+    if content.checked():
+        items = content.get_item_list() if hasattr(content, 'get_item_list') else [content]
+        for item in items:
+            title = item.get_title()
+            yazar = item.get_yazar()
+            img = item.get_img()
+            desc = item.get_desc()
+            content = item.get_main_content()
+            response.add_data(title, yazar, img, desc, content)
+
+    return response.get_response()
+
+
+def process_list_request(html: str, source_id: int):
+    content, response = Builder.build_article_list_request(html, source_id=source_id)
+    return process_request_common(content, response)
+
+
+def process_content_request(html: str, source_id: int):
+    content, response = Builder.build_article_content_request(html, source_id)
+    return process_request_common(content, response)
+
+
+def process_request(r_type: str, content: str, source_id: int):
+    if r_type == 'list':
+        result = process_list_request(content, source_id)
+    else:
+        result = process_content_request(content, source_id)
+
+    return result
 
 
 if __name__ == '__main__':
-    process_list_request()
-    process_content_request()
+    app = Flask(__name__)
+
+
+    @app.route('/content', methods=['POST'])
+    def my_route():
+        data = request.get_json()
+        type_ = data.get('type')
+        content = data.get('content')
+        s_id = data.get('s_id')
+
+        response = process_request(type_, content, s_id)
+        return jsonify(response)
+
+
+    app.run(debug=True)
